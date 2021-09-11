8af87384092b02c88cac25a3fa5a27f93bf1c02f
tushar-kant <tusharkantanayak713@gmail.com>
2021-09-11 21:34:09 +0530

    Updates

---
+++ precache-manifest.faafe5cb7a14ab8486bac3bae9c366b2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3fad3d918c7676733e25b211b6da156",
+    "url": "/ethereum-bank/index.html"
+  },
+  {
+    "revision": "fb1bf3c21c853b859c9e",
+    "url": "/ethereum-bank/static/css/2.5dcc52c6.chunk.css"
+  },
+  {
+    "revision": "e88736dd73af2ca46c0a",
+    "url": "/ethereum-bank/static/css/main.d850c1a7.chunk.css"
+  },
+  {
+    "revision": "fb1bf3c21c853b859c9e",
+    "url": "/ethereum-bank/static/js/2.0ec3ec07.chunk.js"
+  },
+  {
+    "revision": "9143a7c0cbfa518ce39da65db7eaf56a",
+    "url": "/ethereum-bank/static/js/2.0ec3ec07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e88736dd73af2ca46c0a",
+    "url": "/ethereum-bank/static/js/main.ba8df227.chunk.js"
+  },
+  {
+    "revision": "9af8f57fcb4a9dc49b0f",
+    "url": "/ethereum-bank/static/js/runtime-main.3a783565.js"
+  },
+  {
+    "revision": "6255ab96acaa442406b74eed3b80894e",
+    "url": "/ethereum-bank/static/media/dbank1.6255ab96.png"
+  }
+]);
