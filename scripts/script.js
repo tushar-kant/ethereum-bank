d5509cfe2ee14beebd9110fccdbeefd8c2ab44a9
Tushar kanta Nayak <85495427+tushar-kant@users.noreply.github.com>
2022-01-24 20:01:26 +0530

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -2,10 +2,9 @@ const ContractName = artifacts.require("ContractName")
 
 module.exports = async function(callback) {
   try {
-    // Get Accounts
+    
     const accounts = await web3.eth.getAccounts()
     
-    // Fetch the deployed contract
     const contract = await ContractName.deployed()
     console.log('Contract fetched', contract.address)
   }
